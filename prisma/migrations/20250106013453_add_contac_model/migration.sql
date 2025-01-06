ef5792a7e51a2cfaca6fb431ee3572d14f72c4e8
unknown <gilangmgm98@gmail.com>
2025-01-06 08:47:37 +0700

    selesai membuat setup project db logging dan web

---
+++ prisma/migrations/20250106013453_add_contac_model/migration.sql
@@ -0,0 +1,14 @@
+-- CreateTable
+CREATE TABLE `contacts` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `first_name` VARCHAR(100) NOT NULL,
+    `last_name` VARCHAR(100) NULL,
+    `email` VARCHAR(100) NULL,
+    `phone` VARCHAR(20) NULL,
+    `usernames` VARCHAR(100) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `contacts` ADD CONSTRAINT `contacts_usernames_fkey` FOREIGN KEY (`usernames`) REFERENCES `users`(`username`) ON DELETE RESTRICT ON UPDATE CASCADE;
